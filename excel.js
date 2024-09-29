6c7d3a00d3d1efac26b355cf63fe598b08ae4ed0
Brayan Camilo Molina <brayan_cmolina@soy.sena.edu.co>
2024-09-29 17:53:59 -0500

    Create my copy of the code in commit 4

--- excel.js
+++ excel.js
@@ -1,35 +1,42 @@
 const $ = el => document.querySelector(el)
 const $$ = el => document.querySelectorAll(el)
-
+const $table = $('table')
+const $body = $('tbody')
+const $head = $('thead')
 //Filas y columnas 
-
 const ROWS = 30
 const COLUMS = 21
 const FISTR_CHAR = 65
-
-
-
 // Utilidad  dibujar las columnas dentro del body 
 
 const times = length => Array.from({ length }, (_, i)=>i)
 const getColumn = i => String.fromCharCode(FISTR_CHAR + i)
 // variables 
-
 let STATE = times(COLUMS)
-.map(i => times(ROWS).map(j => ({computedValue: '', value: j })))
+.map(i => times(ROWS).map(j => ({computedValue: '', value: '' })))
 
 console.log(STATE)
 
-const renderSpreadsheets = () =>{
-    const $table = $('table')
-    const $body = $('tbody')
-    const $head = $('thead')
+function updateCell ({x, y, value}){
+    //copia profunda del Array STATE 
+    const newState = structuredClone(STATE)
+    const cell = newState[x][y]
+    const computedValue = Number(value)
 
+    cell.value = value //input
+    cell.computedValue = computedValue //Span
+
+    newState[x][y] =cell
+
+    STATE = newState
+
+    renderSpreadsheets()
+}
+
+const renderSpreadsheets = () =>{
     const headerHTML = `<tr>
         <th></th>
-        
         ${times(COLUMS).map(i => `<th>${getColumn(i)}</th>`).join('')}
-    
     </tr>`
     //Se crea las columnas con el codigo de la letra, en este caso dibuja un array desde la posicion 65 = A hasta el valor que determinemos en en la constante COLUMNS
     $head.innerHTML = headerHTML
@@ -48,4 +55,26 @@ const renderSpreadsheets = () =>{
     $body.innerHTML = bodyHTML
 
 }
+$body.addEventListener('click', event =>{
+    const td = event.target.closest('td')
+    if(!td) return
+    const { x, y } = td.dataset
+    const input =td.querySelector('input')
+    const span =td.querySelector('span')
+
+    const end = input.value.length
+    //Pocicion del cursor dentro del input 
+    input.setSelectionRange(end, end)
+    td.classList.add('editing')
+    input.focus()
+    
+    input.addEventListener('blur', () => {
+        console.log({ value: input.value, state: STATE[x][y].value })
+
+        if (input.value === STATE[x][y].value) return
+
+        updateCell({ x, y, value: input.value })
+    //Escuchar el evento en el input una sola vez 
+    },{once: true})
+})
 renderSpreadsheets()
